95adbc59ad33488afef327ea9b840d505e632d20
AtsushiakaRaju <harshasunu2005@gmail.com>
2025-01-28 05:37:25 +0530

    Updated with the comments for better code readablity

--- game.cpp
+++ game.cpp
@@ -1,29 +1,32 @@
+/*Game 1: [Game name], which is an text-based RPG(role playing game)
+with an good storyline.
+The narrator is Eva the retired adventure guide, Who is trying to save the prince from the cursed dragon*/
 #include<iostream>
-#include<string>
-#include<unistd.h>
-#include<random>
+#include<string>  //for strings based functions
+#include<unistd.h> //foe sleep function
+#include<random>  //for random numbers(used in attack and defense)
 using namespace std;
-class game{
+class game{   //parent class which has main logic for the game
   public:
   int phealth;
   string name;
   char c;
   game(){
-      phealth=100;
+      phealth=100;  //health of the player
   }
-  void start(){
+  void start(){   //function which starts the game storyline
     cout<<"Eva: The prince is in very bad condition we need your help!"<<endl; sleep(2);
     cout<<"Eva: Your main objective is to bring back the healing crystal from the cursed dragon cave "<<endl;
-    sleep(2);
+    sleep(2);   //sleep function to add delay which increases the game experience
     cout<<"Eva: Only the healing crystal can save the prince"<<endl; sleep(2);
     cout<<"Eva: To enter the cave , you need to kill the guards and then kill the dragon "<<endl; sleep(2);
   }
-  void attack(int &ehealth){
+  void attack(int &ehealth){   //the main logic for attack and defense in the game
     while( (phealth >0)&&(ehealth>0) ){  
      cout<<"Press 'E' to attack the enemy, or press 'D' to defend "<<endl;
      cin>>c;
      if(c=='E'|| c=='e'){
-         ehealth-=rand() % (25 - 10 + 1) + 10.;
+         ehealth-=rand() % (25 - 10 + 1) + 10;
          cout<<"Your health is: "<<phealth<<endl;
          cout<<"Enemy's health is: "<<ehealth<<endl;
      }
@@ -37,14 +40,14 @@ class game{
 }
 
   }
-  cout << "Congratulations, " << name << "! You've defeated the Goblin!" << endl;sleep(2);
+  cout << "Congratulations, " << name << "! You've defeated the Goblin!" << endl;sleep(2); //congratulation message
   cout<<"Eva: You have killed the enemy "<<endl;sleep(2);
   cout<<"Eva: Next level is comimg soon "<<endl; sleep(2);
   cout<<"Eva: Good luck "<<name<<endl;
   }
 };
-class player:public game{
-    public:
+class player:public game{          //child class to sotre player's details
+    public:   
     void cha(){
         cout<<"Game started"<<endl; sleep(2);
          cout<<"Eva: What should I call you master? "; 
@@ -53,7 +56,7 @@ class player:public game{
         cout<<"Eva: Your health is: "<<phealth<<endl; sleep(2);
     }  
 };
-class lv1:public game{
+class lv1:public game{  //child class to store level 1 enemy details 
     int health;
      public: lv1(){
         health=50;
@@ -65,7 +68,7 @@ class lv1:public game{
          attack(health);
     }  
 };
-int main(){
+int main(){    //main funtion to start the game
     player p;
     p.cha();
     p.start();

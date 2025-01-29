125f509ef21a1c5b8786108da8e0c1b5b0d24102
AtsushiakaRaju <162422898+AtsushiakaRaju@users.noreply.github.com>
2025-01-29 06:59:52 +0530

    Fixed typo's game.cpp

--- game.cpp
+++ game.cpp
@@ -3,7 +3,7 @@ with an good storyline.
 The narrator is Eva the retired adventure guide, Who is trying to save the prince from the cursed dragon*/
 #include<iostream>
 #include<string>  //for strings based functions
-#include<unistd.h> //foe sleep function
+#include<unistd.h> //for sleep function
 #include<random>  //for random numbers(used in attack and defense)
 using namespace std;
 int phealth=100;
@@ -19,7 +19,7 @@ class game{   //parent class which has main logic for the game
     cout<<"Eva: Only the healing crystal can save the prince"<<endl; sleep(2);
     cout<<"Eva: To enter the cave , you need to kill the guards and then kill the dragon "<<endl; sleep(3);
     cout<<"Narrator: Now you have reached the cave where dragon and healing crystal are located "<<endl; sleep(3);
-    cout<<"Narrator: First you need to kill the guards "<<endl; sleep(5);
+    cout<<"Narrator: First you need to defeat the guards "<<endl; sleep(5);
   }
   void attack(int &ehealth, int min, int max){   //the main logic for attack and defense in the game
     while( (phealth >0)&&(ehealth>0) ){  
@@ -45,7 +45,7 @@ class game{   //parent class which has main logic for the game
 void end(){ int h;
     if(phealth>0){
   cout << "Congratulations, " << name << "! You've gained the healing crystal!" << endl;sleep(2); //congratulation message
-  cout<<"Eva: You have killed the enemy "<<endl;sleep(2);
+  cout<<"Eva: You have defeated the enemy "<<endl;sleep(2);
   cout<<"Eva: Next level is comimg soon "<<endl; sleep(2);
   cout<<"Eva: Meet you soon "<<endl; sleep(2);
   cout<<"Eva: Good luck "<<name<<endl;sleep(2);
@@ -57,7 +57,7 @@ void end(){ int h;
       exit(0);
     }}
   else{
-    cout << "Game Over! You have been killed by the enemy." << endl;sleep(3); //game over message
+    cout << "Game Over! You have been defeated by the enemy." << endl;sleep(3); //game over message
     cout<<"Press 1 to play again or 2 to exit: "<<endl; cin>>h;
     if(h==1){
       main();
@@ -99,7 +99,7 @@ class lv2:public game{  //child class to store level 2 enemy details
         cout<<"Narrator: Now you have defeated the goblins "<<endl; sleep(3);
         cout<<"Narrator: Now you need to kill the dragon to aquire the healing crystal"<<endl; sleep(3);
         cout<<"Narrator: As you go deeper into the cave , the strong aura of the dragon grows "<<endl; sleep(3);
-        cout<<"Narrator: You stand in front of the dragon and your job is to kill him and aquire the crystal "<<endl; sleep(5);
+        cout<<"Narrator: You stand in front of the dragon and your job is to kill him and acquire the crystal "<<endl; sleep(5);
         cout<<"Dragon stares at you and starts to talk to you! "<<endl; sleep(2);
          cout<<"Dragon: Ohh! you passed thorugh the gate keeper, "<<name<<endl; sleep(2);
          cout<<"Dragon: You are execptionally good"<<endl; sleep(2);
@@ -112,7 +112,7 @@ int main(){    //main funtion to start the game
     player p;
     p.cha();
     p.start();
-    {lv1 e;
+    {lv1 e;     //bloks{} for optimal usage of memory 
     e.echa();}
     {lv2 e;
     e.echa();}
